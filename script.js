b2b8bc4588d87496c97c5708660efb62591594c6
GabrielFleury <gabrielfleury@hotmail.com>
2019-03-05 06:39:45 -0500

    Brutaliste bulletpoints and cleanup

--- script.js
+++ script.js
@@ -5,14 +5,6 @@ $(function() {
 		$(this).after('<a href='+$url+' target="_blank" class="expand"><img src="../img/expand.png" /></a>');
 	});
 	
-	$width=( $("#content").width() )/2 -25;
-	$( ".expand" ).css( "left", $width+"px" );
-	
-	$(window).resize(function () {
-		$width=( $("#content").width() )/2 -25;
-		$( ".expand" ).css( "left", $width+"px" );
-	}).resize();
-	
 });
 
 
